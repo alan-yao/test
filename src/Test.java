8f64d360ad4a84ededd55e96f26514bc000a77ec
alan-yao <yy1776@nyu.edu>
2016-08-25 01:13:39 -0400

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by 远志 on 8/25/2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        sf;jlksk;fj;laf
+                masterchange
+
+                yyzchange1
+
+    }
+}
